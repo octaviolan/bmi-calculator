e5b1b126b42581dd43e74d4f3993f38f2b3cdf5a
octaviolan <octavio.olan25@gmail.com>
2024-11-14 12:32:08 -0600

    creacion de la clase Units, y clases extendidas metric e imperial

--- js/app.js
+++ js/app.js
@@ -1,11 +1,38 @@
 document.querySelector(".radio").addEventListener("click", (e) => {
   if(e.target.value === 'metric') {
-    document.getElementById('metric').classList.remove('hide');
-    document.getElementById('imperial').classList.add('hide');
+    document.getElementById('metric').style.display = 'block';
+    document.getElementById('imperial').style.display = 'none';
   } else if(e.target.value === 'imperial') {
-    document.getElementById('metric').classList.add('hide');
-    document.getElementById('imperial').classList.remove('hide');
+    document.getElementById('metric').style.display = 'none';
+    document.getElementById('imperial').style.display = 'block';
   } else {
     return;
   }
 });
+
+class Units {
+  constructor(type) {
+    this.type = type;
+    this.elementsAll = [...document.querySelectorAll(`.input-${type}`)];
+  }
+}
+
+class Metric extends Units {
+  constructor() {
+    super();
+    this.elements = new Units('metric').elementsAll;
+    console.log(this.elements)
+  }
+}
+
+class Imperial extends Units {
+  constructor() {
+    super();
+    this.elements = new Units('imperial').elementsAll;
+    console.log(this.elements)
+  }
+}
+
+const metric = new Metric();
+const imperial = new Imperial();
+
