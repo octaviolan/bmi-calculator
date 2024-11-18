ed9ffd303683462ae96b982fb7144855f185405a
octaviolan <octavio.olan25@gmail.com>
2024-11-17 18:26:43 -0600

    Creando metodo weightClassificarion y healthyWeight en la clase padre

--- js/app.js
+++ js/app.js
@@ -1,75 +1,101 @@
 //Seleccionar el tipo de sistema de medicion (metric o imperial)
 document.querySelector(".radio").addEventListener("click", (e) => {
-  const metricElement = document.getElementById('metric');
-  const imperialElement = document.getElementById('imperial');
-  const welcomeElement = document.querySelector('.welcome');
-  const outputContainer = document.querySelector('.output-container');
+  //Acceder a los elementos del DOM
+  const metricElement = document.getElementById("metric");
+  const imperialElement = document.getElementById("imperial");
+  const welcomeElement = document.querySelector(".welcome");
+  const outputContainer = document.querySelector(".output-container");
   const inputTypeNumber = document.querySelectorAll("input[type='number']");
 
-  if(e.target.value === 'metric' || e.target.value === 'imperial') {
-    metricElement.style.display = e.target.value === 'metric' ? 'block' : 'none';
-    imperialElement.style.display = e.target.value === 'imperial' ? 'block' : 'none';
+  //Condicional que muestra u oculta el contenedor metric o imperial
+  if (e.target.value === "metric" || e.target.value === "imperial") {
+    metricElement.style.display =
+      e.target.value === "metric" ? "block" : "none";
+    imperialElement.style.display =
+      e.target.value === "imperial" ? "block" : "none";
 
     //Mostrar siempre seccion welcome y ocultar resultados bmi
-    welcomeElement.style.display = 'block';
-    outputContainer.style.display = 'none'
+    welcomeElement.style.display = "block";
+    outputContainer.style.display = "none";
 
     //limpiar inputs
-    for(input of inputTypeNumber) {
-      console.log(input.value);
-      input.value = '';
+    for (input of inputTypeNumber) {
+      input.value = "";
     }
   }
 });
 
 //Clase Units
 class Units {
-  constructor() {
-    
-  }
+  constructor() {}
   getElements(type) {
     return [...document.querySelectorAll(`.input-${type}`)];
   }
   showBmi(bmi) {
     //Mostrar siempre seccion resultados y ocultar welcome
-    document.querySelector('.welcome').style.display = 'none';
-    document.querySelector('.output-container').style.display = 'flex';
-    
+    document.querySelector(".welcome").style.display = "none";
+    document.querySelector(".output-container").style.display = "flex";
+
     //Mostar bmi
-    document.getElementById('bmi').textContent = bmi;
-
-    //Clasificacion de resultados
-    if(bmi < 18.5) {
-      document.getElementById('classification').textContent = 'Underweight';
-    } else if (bmi >= 18.5 && bmi < 24.9) {
-      document.getElementById('classification').textContent = 'Healthy weight';
-    } else if (bmi >= 25 && bmi < 29.9) {
-      document.getElementById('classification').textContent = 'Overweight';
-    } else if(bmi > 30){
-      document.getElementById('classification').textContent = 'Obese';
-    }    
+    document.getElementById("bmi").textContent = bmi;
   }
+  weightClassification(bmi) {
+    //Clasificacion de resultados de acuerdo al BMI
+    if (bmi > 30) {
+      document.getElementById("classification").textContent = "Obese";
+    } else if (bmi > 25) {
+      document.getElementById("classification").textContent = "Overweight";
+    } else if (bmi > 18.5) {
+      document.getElementById("classification").textContent = "Healthy weight";
+    } else if (bmi > 0) {
+      document.getElementById("classification").textContent = "Underweight";
+    }
+  }
+  healthyWeight(height) {
+    //Bmi minimo y maximo
+    const lowerBmi = 18.5;
+    const upperBmi = 25;
 
+    //Formula que calcula el peso minimo y maximo
+    const lowerWeight = lowerBmi * Math.pow(height / 100, 2);
+    const upperWeight = upperBmi * Math.pow(height / 100, 2);
+
+    //Redondear peso a un decimal
+    const lowerWeightFixed = lowerWeight.toFixed(1);
+    const upperWeightFixed = upperWeight.toFixed(1);
+
+    //Mostar los resultados en el DOM
+    document.getElementById(
+      "range"
+    ).textContent = `${lowerWeightFixed}kgs - ${upperWeightFixed}kgs`;
+  }
 }
 
 //Subclase metric
 class Metric extends Units {
   constructor() {
     super();
-    this.elements = this.getElements('metric');
+    this.elements = this.getElements("metric");
 
-    this.elements.forEach(element => {
-      element.addEventListener('input', this.getBmiMetric.bind(this))
-    })
-    
+    this.elements.forEach((element) => {
+      element.addEventListener("input", this.getBmiMetric.bind(this));
+    });
   }
   getBmiMetric() {
     const [cm, kg] = this.elements;
-    if(cm.value !== '' && kg.value !== '') {
-      const bmiMetric = kg.value / Math.pow((cm.value / 100), 2)
+
+    if (cm.value !== "" && kg.value !== "") {
+      const bmiMetric = kg.value / Math.pow(cm.value / 100, 2);
       const bmiMetricFixed = bmiMetric.toFixed(1);
-      console.log({cm: cm.value, kg: kg.value, bmiMetric: bmiMetric.toFixed(1)});
+      console.log({
+        cm: cm.value,
+        kg: kg.value,
+        bmiMetric: bmiMetric.toFixed(1),
+      });
+
       this.showBmi(bmiMetricFixed);
+      this.weightClassification(bmiMetricFixed);
+      this.healthyWeight(cm.value);
     }
   }
 }
@@ -78,12 +104,10 @@ class Metric extends Units {
 class Imperial extends Units {
   constructor() {
     super();
-    this.elements = this.getElements('imperial');
-  
+    this.elements = this.getElements("imperial");
   }
 }
 
 //Instancias
 const metric = new Metric();
 const imperial = new Imperial();
-
